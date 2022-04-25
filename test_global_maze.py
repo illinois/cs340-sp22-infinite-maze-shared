3cf1b97f7536e767d821a99911382e82ff732ac0
PeterYR <ziyuzhang01@gmail.com>
2022-04-25 00:00:53 -0500

    another test case just to be sure

--- test_global_maze.py
+++ test_global_maze.py
@@ -1,3 +1,4 @@
+import random
 from global_maze import GlobalMaze
 
 def test_simple_set_get():
@@ -15,3 +16,16 @@ def test_reset():
     maze_state.reset()
     assert maze_state.get_state(0, 0) == None, 'Segment at origin exists despite reset.'
     assert maze_state.get_full_state() == {}, 'Maze state not empty despite reset.'
+
+def test_many_segments():
+    maze_state = GlobalMaze()
+
+    expected_states = {}
+    for _ in range(100):
+        expected_states[(random.randint(-100, 100), random.randint(-100, 100))] = {random.random(): random.random()}
+    
+    for key, val in expected_states.items():
+        maze_state.set_state(key[0], key[1], val)
+    
+    for key, val in expected_states.items():
+        assert maze_state.get_state(key[0], key[1]) == val, 'Retrieved state did not match expected.'
