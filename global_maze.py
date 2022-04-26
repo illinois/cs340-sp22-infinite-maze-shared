ec819d53d30af30d3f37cb5d2ab7612d94c6d360
PeterYR <ziyuzhang01@gmail.com>
2022-04-26 18:47:10 -0500

    add get_free_space method

--- global_maze.py
+++ global_maze.py
@@ -25,3 +25,12 @@ class GlobalMaze:
     def is_empty(self) -> bool:
         '''Return `True` if no data is present, else return `False`'''
         return self.__is_empty
+    
+    def get_free_space(self, row: int, col: int, radius: int) -> list:
+        '''Return list of free spaces in given radius centered on given coords'''
+        output = []
+        for r in range(row - radius, row + radius + 1):
+            for c in range(col - radius, col + radius + 1):
+                if (r, c) not in self.__state.keys():
+                    output.append((r, c))
+        return output
