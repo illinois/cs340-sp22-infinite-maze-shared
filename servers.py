eb38f949fa0f0efbd076621ae65fda99e8f81701
Aarya Bhatia <aarya.bhatia1678@gmail.com>
2022-04-28 12:38:02 -0500

    hide connection from app

--- servers.py
+++ servers.py
@@ -1,14 +1,15 @@
-from numpy import insert
 from connection import Connection
 from maze import *
 import random
 # import validators
 
+
 class ServerManager:
-    def __init__(self, connection: Connection):
-        self.names = [] # list of server names for random.choices
-        self.weights = [] # list of server weights for random.choices
-        self.servers = {} # cache: Maps server name to data 
+    def __init__(self, db_name):
+        self.connection = Connection(db_name)  # database connection
+        self.names = []  # list of server names for random.choices
+        self.weights = []  # list of server weights for random.choices
+        self.servers = {}  # cache: Maps server name to data
 
         # Fetch all servers from db
         try:
@@ -27,32 +28,20 @@ class ServerManager:
         """Inserts a server to Db and updates the names, weights and servers data of the class for new server.
 
         Args:
-            data (dict): Server data must contain the name, url and author field. It can optionally have a weight between 0 and 1.
+            data (dict): Server data containing name, url, author and weight
 
         Returns:
             tuple: (status code: int, error message: str)
-            
+
             Status Codes
             -------------
             201: Success
-            400: Invalid Data
+            400: User Error
             500: Database Error
             -------------
         """
-        if 'name' not in data or 'url' not in data:
-            return 400, "name or url invalid"
-
-        # if not validators.url(data['url']):
-        #     return 400, "url is invalid"
-
-        if 'weight' not in data:
-            data['weight'] = 1
-        
-        if data['weight'] < 0 or data['weight'] > 1:
-            return 400, "weight is invalid"
-        
         if self.servers and data['name'] in self.servers:
-            return 400, "duplicate server name"
+            return 400, "Duplicate Name Error"
 
         result = self.connection.add_server(data)
 
@@ -60,9 +49,13 @@ class ServerManager:
 
         if result:
             id = result.inserted_id
-            data['_id'] = Connection.stringify_id(id)
-            data['index'] = len(self.names) # index at which name and weight is stored in the arrays
-            self.servers[data['name']] = data 
+
+            data['_id'] = Connection.stringify_id(id)  # store database ID
+
+            # index at which name and weight is stored in the arrays
+            data['index'] = len(self.names)
+
+            self.servers[data['name']] = data
             self.names.append(data['name'])
             self.weights.append(data['weight'])
 
@@ -71,17 +64,27 @@ class ServerManager:
         return 500, "Database Error"
 
     def remove(self, name):
+        """Removes a server with given name
+
+        Args:
+            name (str): Name of server
+
+        Returns:
+            tuple: (status code, error message)
+        """
         if name not in self.servers:
-            return "", 400
-        
+            return 400, ""
+
         self.connection.remove_server(self.servers['_id'])
+
+        # Update cache
 
         index = self.servers[name]
         del self.servers[name]
         self.names.pop(index)
         self.weights.pop(index)
 
-        return "", 200
+        return 200, ""
 
     def find(self, name):
         return self.servers[name]
@@ -99,39 +102,46 @@ class ServerManager:
         if name not in self.servers:
             return 400, "Server does not exist"
 
+        result = self.connection.update_server(self.servers[name]["_id"], data)
+
+        if result.modified_count == 0:
+            return 500, "Database Error"
+
+        # Update cache
+
         for key in data:
             self.servers[name][key] = data[key]
-        
-        result = self.connection.update_server(self.servers[name]["_id"], data)
 
         index = self.servers[name]
 
         if 'weight' in data:
             self.weights[index] = data['weight']
-        
+
         if 'name' in data:
             self.names[index] = data['name']
             self.servers[data['name']] = self.servers[name]
             del self.servers[name]
 
-        if result.modified_count == 0:
-            return 500, "Database Error"
-
         return 200, ""
 
-    def select_random(self) -> dict:
+    def select_random(self) -> str:
         """Select random server from available ones and return it
 
         Returns:
-            dict: server data, or None if there are none
+            str: Name of server, or None if none
         """
         if not self.servers:
             return None
-            
+
         mg_name = random.choices(
-            population = self.names,
-            weights = self.weights,
-            K = 1
+            population=self.names,
+            weights=self.weights,
+            K=1
         )
 
-        return self.servers[mg_name]
+        return mg_name
+
+    def has_servers(self):
+        """Returns True if there are any available servers, otherwise returns False.
+        """
+        return len(self.servers) > 0
